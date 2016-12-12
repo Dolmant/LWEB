39674acd8434d98254fc354136d1fb37a139ea00
Dylan Simmer <goldenoblivion@gmail.com>
2016-12-12 23:16:18 +1100

    committing new node modules

---
+++ assets/jssource/assets.js
@@ -0,0 +1,151 @@
+import React from 'react';
+
+export function animals() {
+	return (
+		[
+			<li>
+				<a href="">
+					<div className="img-wrap">
+	                    <img src="./assets/images/Animals/Binturong.jpg"></img>
+					</div>
+					<span>Binturong</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+	                    <img src="./assets/images/Animals/BlackBear.jpg"></img>
+					</div>
+					<span>BlackBear</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Animals/RedPanda.jpg"></img>
+					</div>
+					<span>RedPanda</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Insects/DragonflySigned.jpg"></img>
+					</div>
+					<span>Dragonfly</span>
+				</a>
+			</li>,
+		]
+	);
+}
+
+export function science() {
+	return (
+		[
+			<li>
+				<a href="">
+					<div className="img-wrap">
+	                    <img src="./assets/images/Science/mouse02 copy.png"></img>
+					</div>
+					<span>Binturong</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+	                    <img src="./assets/images/Science/3D/06_Cell_Internal.png"></img>
+					</div>
+					<span>3D Cell internals</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Science/Medical Illustration/CatAnatomy2.jpg"></img>
+					</div>
+					<span>Cat Anatomy</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Science/Medical Illustration/COPD2_sign.jpg"></img>
+					</div>
+					<span>COPD2</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Science/Medical Illustration/Hand_Anatomy02.png"></img>
+					</div>
+					<span>Hand Anatomy</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Science/Medical Illustration/StomachLayers_01.jpg"></img>
+					</div>
+					<span>Stomach Layers</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Science/Typography/Blood_Components2.jpg"></img>
+					</div>
+					<span>Blood Components</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Science/Typography/Micro2.jpg"></img>
+					</div>
+					<span>Micro</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Science/Typography/Neuroscience.jpg"></img>
+					</div>
+					<span>Neuroscience</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Science/Typography/Proteomics_Typo_02.jpg"></img>
+					</div>
+					<span>Proteomics</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Science/VizbiPlus/08_Vizbi_DataTransfer.jpg"></img>
+					</div>
+					<span>Data Transfer</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Science/VizbiPlus/09_Vizbi_WINNER_FibrilForest.jpg"></img>
+					</div>
+					<span>Fibril Forest</span>
+				</a>
+			</li>,
+			<li>
+				<a href="">
+					<div className="img-wrap">
+						<img src="./assets/images/Science/VizbiPlus/FriesWithThat3.png"></img>
+					</div>
+					<span>Fries With That?</span>
+				</a>
+			</li>,
+		]
+	);
+}
