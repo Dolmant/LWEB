4fa6a3882f61585fd535e4c5214fd35f1dbfdfe3
Dylan Simmer <goldenoblivion@gmail.com>
2016-12-27 00:10:48 +1100

    gruntfilefix

--- gruntfile.js
+++ gruntfile.js
@@ -1,43 +1,48 @@
 module.exports = function (grunt) {
-   grunt.initConfig({
-      browserify: {
-         dist: {
-            options: {
-               transform: [
-                  ["babelify", {
-                     loose: "all"
-                  }]
-               ]
-            },
-            files: {
-               // if the source file has an extension of es6 then
-               // we change the name of the source file accordingly.
-               // The result file's extension is always .js
-               "./assets/js/bundle.js": ["./assets/jssource/hightide.js"],
-            }
-         }
-      },
-      sass: {
-         dist: {
-            files: {
-               "./assets/css/style.css": ["./assets/scss/style.scss"]
-            }
-         }
-      },
+	grunt.initConfig({
+		browserify: {
+			dist: {
+				options: {
+					transform: [
+						['babelify', {
+							loose: 'all',
+						}],
+					],
+				},
+				files: {
+					// if the source file has an extension of es6 then
+					// we change the name of the source file accordingly.
+					// The result file's extension is always .js
+					'./assets/js/bundle.js': ['./assets/jssource/hightide.js'],
+				},
+			},
+		},
+		sass: {
+			dist: {
+				files: {
+					'./assets/css/style.css': ['./assets/scss/style.scss'],
+				},
+			},
+		},
 
-      watch: {
-         scripts: {
-            files: ["./assets/jssource/*.js", "./assets/scss/*.scss"],
-            tasks: ["browserify", "sass"]
-         }
-      }
-   });
+		watch: {
+			watchcss: {
+				files: ['./assets/scss/*.scss'],
+				tasks: ['sass'],
+			},
+			watchjs: {
+				files: ['./assets/jssource/*.js'],
+				tasks: ['browserify'],
+			},
+		},
+	});
 
-   grunt.loadNpmTasks("grunt-browserify");
-   grunt.loadNpmTasks("grunt-contrib-watch");
-   grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 
-   grunt.registerTask("default", ["watch"]);
-   grunt.registerTask("build", ["browserify", "sass"]);
-   grunt.registerTask("build_sass", ["sass"]);
+	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('build', ['browserify', 'sass']);
+	grunt.registerTask('build_sass', ['sass']);
+	grunt.registerTask('test', ['watchcss', 'watchjs']);
 };
