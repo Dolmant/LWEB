c699b39de0b494cecc95b7483d88e776e955bd62
Dylan Simmer <goldenoblivion@gmail.com>
2020-02-04 18:28:40 +1100

    scaffolding

--- lweb.ts
+++ lweb.ts
@@ -1,46 +1,75 @@
 import config from "./config"
 
-const loadImage = (targetEl: HTMLElement, source: string) => {
+interface assetItem {
+  file: string
+  files?: string[]
+  text: string
+}
+
+const loadImage = (targetEl: HTMLElement, source: assetItem) => {
   // Loads image into target el, displays loading graphic while its fetching
   switch(true) {
-    case (source.includes(".mp4")): {
+    case (source.file.includes(".mp4")): {
       // todo
       break
     }
-    case (source.includes(".jpg")): {
+    case (source.file.includes(".gif")): {
       // todo
       break
     }
-    case (source.includes(".gltf")): {
+    case (source.file.includes(".jpg")): {
+      // todo
+      if (source.files) {
+        // Attach different stuff
+        break
+      }
+      break
+    }
+    case (source.file.includes(".gltf")): {
       // todo
       break
     }
     default: {
-      console.log("Error, unknown file type: " + source)
+      console.log("Error, unknown file type: " + source.file)
     }
   }
 }
-const highDefImage = (targetEl: HTMLImageElement, source: string) => {
+const highDefImage = (targetEl: HTMLImageElement, source: assetItem) => {
   // Loads high def version, incrementally streams update
   switch(true) {
-    case (source.includes(".mp4")): {
+    case (source.file.includes(".gif")): {
       // todo
       break
     }
-    case (source.includes(".jpg")): {
+    case (source.file.includes(".mp4")): {
       // todo
       break
     }
-    case (source.includes(".gltf")): {
+    case (source.file.includes(".jpg")): {
+      // todo
+      break
+    }
+    case (source.file.includes(".gltf")): {
       // todo
       break
     }
     default: {
-      console.log("Error, unknown file type: " + source)
+      console.log("Error, unknown file type: " + source.file)
     }
   }
 }
+Object.keys(config).forEach(category => {
+  // todo iterate and load
+})
 
 window.onscroll = () => {
   // Hook into scroll of elements. If at end, add extra images to the element
+}
+
+// Attach handlers
+const menuItem = document.getElementById("")
+if (menuItem) {
+  menuItem.onclick = ((ev: MouseEvent) => {
+    // todo mark classes/display none on stuff
+  })
 }
