805becedb5bddd36f291181f792541ce8518093a
Dylan Simmer <goldenoblivion@gmail.com>
2020-02-04 18:12:49 +1100

    Add bundler

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 magick mogrify -resize 1920x1080 -quality 100 -path ./processedImages/HQ ./assets/*.jpg
 magick mogrify -resize 500x500 -quality 50 -path ./processedImages/thumbs ./assets/*.jpg
-tsc
+npm i
+parcel build ./lweb.ts
--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-{
+export default {
   "Nature": [
     {
       "file": "DragonPoster.jpg",
